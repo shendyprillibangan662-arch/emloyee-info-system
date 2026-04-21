aae138629783feaea857f8a621c91563e747767d
SHEN <shendyprillibangan662@gmail.com>
2026-04-21 12:14:36 +0800

    Revert "added backend"

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-student-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'employee.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
